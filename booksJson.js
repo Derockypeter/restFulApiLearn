8a6bf3a0d55aba7c670d41129d85a41201044f19
Amaizu Somto Peter <amaizupeter@yahoo.com>
2021-03-28 23:02:06 +0100

    Node BOOK API

---
+++ booksJson.js
@@ -0,0 +1,32 @@
+db.books.insert([
+    {
+        title: "Becoming A Better You",
+        genre: "Self Development",
+        author: "Joel Osteen",
+        read: false
+    },
+    {
+        title: "No More Sheets",
+        genre: "Self Development",
+        author: "Nikita Oswell",
+        read: false
+    },
+    {
+        title: "Bitches and Wives",
+        genre: "Self Development",
+        author: "Marudo Taylor",
+        read: false
+    },
+    {
+        title: "Hayes",
+        genre: "History",
+        author: "Charles murdock",
+        read: false
+    },
+    {
+        title: "God of impossiblity",
+        genre: "Religion",
+        author: "Charles murdock",
+        read: false
+    }
+]);
